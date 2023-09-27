81340f46d62e60c5e928ba78ccbb0d04057df368
Moosa <computerknowledge817@gmail.com>
2023-09-28 00:18:44 +0500

    allowed host to .vercel.app

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = getenv('SECRET_KEY')
 DEBUG = bool(getenv('DEBUG'))
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
-                 '192.168.18.123', 'talktopia.vercel.app']
+                 '192.168.18.123', 'talktopia.vercel.app', '.vercel.app']
 
 
 # Application definition
