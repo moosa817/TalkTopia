4f99a356e47a5e15479307eb8231f0a168ffbb07
Moosa <computerknowledge817@gmail.com>
2023-10-03 00:04:42 +0500

    changed asgi.py

--- chatroom/asgi.py
+++ chatroom/asgi.py
@@ -6,18 +6,18 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
+import os
 from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatroom.settings')
 
 django_asgi_app = get_asgi_application()
-
+app = django_asgi_app
 
 import base.routing
-import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatroom.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 
@@ -30,4 +30,3 @@ application = ProtocolTypeRouter({
     )
 })
 
-app = application
