d7473886ab6816aaccb6769f7919a8e8e1ce2a54
Moosa <computerknowledge817@gmail.com>
2023-10-12 00:44:58 +0500

    registered websocketsessions on admin.py

--- base/admin.py
+++ base/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Room, Message, Topic, UserProfile
+from .models import Room, Message, Topic, UserProfile, WebsocketSessions
+
 # from .models import User
 # # Register your models here.
 
 admin.site.register(Room)
 admin.site.register(Message)
 admin.site.register(Topic)
-
+admin.site.register(WebsocketSessions)
 
 admin.site.register(UserProfile)
--- chatroom/urls.py
+++ chatroom/urls.py
@@ -15,11 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('base.urls'))
-]
+urlpatterns = [path("admin817/", admin.site.urls), path("", include("base.urls"))]
