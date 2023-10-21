063803a7f8b70cc27815ef08dcc46b1f1ccb2e92
Moosa <computerknowledge817@gmail.com>
2023-10-21 21:49:42 +0500

    setted message empty=False

--- base/models.py
+++ base/models.py
@@ -77,7 +77,7 @@ class Room(models.Model):
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey("Room", on_delete=models.CASCADE)
-    body = models.TextField()
+    body = models.TextField(blank=False)
     edited = models.BooleanField(default=False)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
